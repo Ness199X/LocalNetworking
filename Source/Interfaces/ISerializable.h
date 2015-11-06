82a57e1fd6ea3540505eb04f16a12693753caced
Convery <Convery10122@gmail.com>
2015-11-06 09:20:49 +0100

    Base for serializable objects.

---
+++ Source/Interfaces/ISerializable.h
@@ -0,0 +1,25 @@
+/*
+    Initial author: (https://github.com/)Convery
+    License: LGPL 3.0
+    Started: 2015-11-06
+    Notes:
+        A basic datatype representing an object that can be serialized.
+        This datatype is base for most data sent.
+*/
+
+#pragma once
+
+class ISerializable
+{
+public:
+    virtual void Serialize(class ByteBuffer *Buffer);
+    virtual void Deserialize(class ByteBuffer *Buffer);
+};
+
+inline void ISerializable::Serialize(class ByteBuffer *Buffer)
+{
+
+}
+inline void ISerializable::Deserialize(class ByteBuffer *Buffer)
+{
+}
