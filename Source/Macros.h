bc5192876b65262ad68daa855cb6244af63cf7bf
Convery <Convery10122@gmail.com>
2015-11-10 04:07:43 +0100

    C style macros for easier printing.

--- Source/Macros.h
+++ Source/Macros.h
@@ -7,3 +7,16 @@
 */
 
 #pragma once
+#include "Defines.h"
+#include "Utility\String\DebugString.h"
+
+// Debug information.
+#ifdef NDEBUG
+#define PrintFunction()
+#define DebugPrint(string)
+#define InfoPrint(string)
+#else
+#define PrintFunction() AppendToLogfile(__func__, "Func")
+#define DebugPrint(string) AppendToLogfile(string, "Debug")
+#define InfoPrint(string) AppendToLogfile(string, "Info")
+#endif
